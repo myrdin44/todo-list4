31c23d4109499ea00ec7f28b1addbb3d5f7798db
UzzjyLeol <nguyenvanbacahihi@gmail.com>
2024-12-30 13:18:46 +0700

    update endpoint

--- src/main/java/org/example/todolist/controller/TestController.java
+++ src/main/java/org/example/todolist/controller/TestController.java
@@ -5,11 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/secure")
+@RequestMapping("/test-possible-deployment")
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/is-deploy")
     public ResponseEntity<String> testSecureEndpoint() {
-        return ResponseEntity.ok("Secure endpoint accessed successfully");
+        return ResponseEntity.ok("Deploy succesfully!s");
     }
 }

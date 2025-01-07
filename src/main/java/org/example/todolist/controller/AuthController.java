7513466890bae5c236451d59ec8da089f4dc819e
UzzjyLeol <nguyenvanbacahihi@gmail.com>
2025-01-07 23:36:01 +0700

    update cors

--- src/main/java/org/example/todolist/controller/AuthController.java
+++ src/main/java/org/example/todolist/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("https://bigdragonusermanagementapp.netlify.app")
+@CrossOrigin(value = "https://bigdragonusermanagementapp.netlify.app")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
---
+++ src/test/java/org/example/todolist/controller/AuthControllerTest.java
@@ -0,0 +1,21 @@
+package org.example.todolist.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void login() {
+    }
+}

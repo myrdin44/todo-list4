6043ea38657488d7af9fc74b546f29dc4f0206aa
UzzjyLeol <nguyenvanbacahihi@gmail.com>
2025-01-07 22:51:45 +0700

    update cors

--- src/main/java/org/example/todolist/config/WebConfig.java
+++ src/main/java/org/example/todolist/config/WebConfig.java
@@ -1,29 +1,17 @@
 package org.example.todolist.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**") // ap dung cho tat ca endpoint
-//        .allowedOrigins("https://bigdragonusermanagementapp.netlify.app") //set port duoc cho phep
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://bigdragonusermanagementapp.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-            }
-        }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // ap dung cho tat ca endpoint
+        .allowedOrigins("https://bigdragonusermanagementapp.netlify.app") //set port duoc cho phep
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
--- src/main/java/org/example/todolist/controller/AuthController.java
+++ src/main/java/org/example/todolist/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
